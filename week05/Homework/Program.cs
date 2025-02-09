58e18c9f38bfdfb23a6538a1078e359ba124d738
deark736 <dearken618@gmail.com>
2025-02-09 22:38:00 +0800

    Implement inheritance for Assignment classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
